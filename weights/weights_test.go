937cfa77457e223dc3612d5339282167f1e4beea
oursimon <simon@ourstudio.se>
2025-08-29 08:41:16 +0200

    builder concat test

--- weights/weights_test.go
+++ weights/weights_test.go
@@ -3,6 +3,8 @@ package weights
 import (
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSelections_extractActiveSelections(t *testing.T) {
@@ -182,4 +184,24 @@ func TestCreate(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_Weights_Concat(t *testing.T) {
+	w1 := Weights{
+		"x": 1,
+		"y": 2,
+	}
+
+	w2 := Weights{
+		"z": 3,
+	}
+
+	want := Weights{
+		"x": 1,
+		"y": 2,
+		"z": 3,
+	}
+
+	w1.Concat(w2)
+	assert.Equal(t, want, w1)
 }
