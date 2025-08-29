a9769f79e819f0e84c250858494aa98fa1d3c74a
oursimon <simon@ourstudio.se>
2025-08-29 09:02:14 +0200

    unittests for utils

---
+++ utils/slices_test.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ourstudio-se/puan-sdk-go/fake"
+)
+
+func Test_Without(t *testing.T) {
+	sliceA := []int{1, 2, 3, 4, 5}
+	sliceB := []int{2, 4}
+	want := []int{1, 3, 5}
+
+	actual := Without(sliceA, sliceB)
+
+	assert.Equal(t, want, actual)
+}
+
+func Test_Without_nilComparable_shouldReturnSameSlice(t *testing.T) {
+	sliceA := fake.New[[]int]()
+	actual := Without(sliceA, nil)
+
+	assert.Equal(t, sliceA, actual)
+}
+
+func Test_Without_nilInput_shouldReturnEmptySlice(t *testing.T) {
+	sliceB := fake.New[[]int]()
+	actual := Without(nil, sliceB)
+	var want []int
+
+	assert.Equal(t, want, actual)
+}
