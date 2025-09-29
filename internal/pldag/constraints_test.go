014433150a8d18eefc82811aad928a26ef185a49
oursimon <simon@ourstudio.se>
2025-09-29 16:25:20 +0200

    rename in test

--- internal/pldag/constraints_test.go
+++ internal/pldag/constraints_test.go
@@ -212,12 +212,12 @@ func Test_newConstraintID(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			actual, err := newConstraintID(tt.coefficients, tt.bias)
+			got, err := newConstraintID(tt.coefficients, tt.bias)
 			assert.NoError(t, err)
 			assert.Equalf(
 				t,
 				tt.want,
-				actual,
+				got,
 				"newConstraintID(%v, %v)",
 				tt.coefficients,
 				tt.bias,
