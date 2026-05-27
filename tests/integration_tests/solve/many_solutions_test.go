5b841a6addff5876c36bd5d4beb56ab4e7b26be1
Oscar Blomqvist <oscar.blomqvist@gmail.com>
2026-05-27 08:46:01 +0200

    Add integration test for mixed dependent and independent selections

--- tests/integration_tests/solve/many_solutions_test.go
+++ tests/integration_tests/solve/many_solutions_test.go
@@ -14,7 +14,7 @@ import (
 // Create selections for all, some of which are composite.
 // The solver should create a solution for each selection.
 // nolint:lll
-func Test_CreateSolutionsBySelection_givenManyDependentSelections_shouldCreateSolutionForEachSelection(
+func Test_CreateSolutionsBySelection_givenManyDependentSelections_shouldCreateSolutionForEach(
 	t *testing.T,
 ) {
 	creator := puan.NewRulesetCreator()
@@ -36,6 +36,7 @@ func Test_CreateSolutionsBySelection_givenManyDependentSelections_shouldCreateSo
 
 	ruleset, _ := creator.Create()
 
+	// Make half of the selections composite.
 	selections := make([]puan.Selection, len(primitives))
 	for i, primitive := range primitives {
 		builder := puan.NewSelectionBuilder(primitive)
@@ -61,7 +62,7 @@ func Test_CreateSolutionsBySelection_givenManyDependentSelections_shouldCreateSo
 // Ruleset with many independent primitives.
 // The solver should create a solution for each selection.
 // nolint:lll
-func Test_CreateSolutionsBySelection_givenManyIndependentSelections_shouldCreateSolutionForEachSelection(
+func Test_CreateSolutionsBySelection_givenManyIndependentSelections_shouldCreateSolutionForEach(
 	t *testing.T,
 ) {
 	creator := puan.NewRulesetCreator()
@@ -84,6 +85,58 @@ func Test_CreateSolutionsBySelection_givenManyIndependentSelections_shouldCreate
 	for i, primitive := range primitives {
 		selections[i] = puan.NewSelectionBuilder(primitive).Build()
 	}
+
+	solutions, _ := solutionCreator.CreateSolutionsBySelection(selections, ruleset, nil)
+
+	assert.Len(t, solutions.SolutionsBySelection(), len(primitives))
+	for _, selection := range selections {
+		solution, err := solutions.GetSolutionBySelection(selection)
+
+		assert.NoError(t, err)
+		newSolutionAsserter(solution.Solution()).
+			assertActive(t, selection.IDs()...)
+	}
+}
+
+// Ruleset with many dependent and independent primitives.
+// The solver should create a solution for each selection.
+// nolint:lll
+func Test_CreateSolutionsBySelection_givenMixedSelections_shouldCreateSolutionForEach(
+	t *testing.T,
+) {
+	creator := puan.NewRulesetCreator()
+	from := time.Now()
+	end := from.Add(1 * time.Hour)
+	_ = creator.EnableTime(from, end)
+
+	dependentPrimitives := fake.New[[]string](
+		func(oo *options.Options) {
+			oo.RandomMinSliceSize = 40
+			oo.RandomMaxSliceSize = 40
+		},
+	)
+	independentPrimitives := fake.New[[]string](
+		func(oo *options.Options) {
+			oo.RandomMinSliceSize = 40
+			oo.RandomMaxSliceSize = 40
+		},
+	)
+
+	var primitives []string
+	primitives = append(primitives, dependentPrimitives...)
+	primitives = append(primitives, independentPrimitives...)
+
+	_ = creator.AddPrimitives(primitives...)
+
+	orID, _ := creator.SetOr(dependentPrimitives...)
+	_ = creator.Assume(orID)
+
+	selections := make([]puan.Selection, len(primitives))
+	for i, primitive := range primitives {
+		selections[i] = puan.NewSelectionBuilder(primitive).Build()
+	}
+
+	ruleset, _ := creator.Create()
 
 	solutions, _ := solutionCreator.CreateSolutionsBySelection(selections, ruleset, nil)
 
